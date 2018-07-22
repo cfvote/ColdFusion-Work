d0140ff8e2d8223238d520a4ab956f3e08f26b5d
Barrett Otte <barrettotte@gmail.com>
2018-07-22 14:18:58 -0400

    Add files via upload

--- Batch Scripts/CF-Server.bat
+++ Batch Scripts/CF-Server.bat
@@ -3,9 +3,9 @@ REM Stops then Starts ColdFusion Server
 CLS
 
 REM https://stackoverflow.com/questions/7809648/get-display-name-of-current-windows-domain-user-from-a-command-prompt
+REM NOTE: Does not work if not connected to work network, must set DNAME manually if not connected.
 SET TNAME="net user %USERNAME% /domain| FIND /I "Full Name""
 FOR /F "tokens=3,4 delims=, " %%A IN ('%TNAME%') DO SET DNAME=%%A
-
 SET CF_BIN=C:\ColdFusion11\%DNAME%\bin\
 
 IF NOT EXIST %CF_BIN% CALL:NOTFOUND "ColdFusion bin folder" %CF_BIN% "CF_BIN"
